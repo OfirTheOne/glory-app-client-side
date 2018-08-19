cdd28b45f9d95eba92c389af8f4cb517fc6db122
TheCodeSleyer <ofir3322@gmail.com>
2018-08-19 20:23:53 +0300

    general commit

---
+++ src/components/stripe-payment-form/stripe-payment-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { StripePaymentFormComponent } from './stripe-payment-form.component';
+
+@NgModule({
+  declarations: [
+    StripePaymentFormComponent,
+  ],
+  imports: [],
+  exports: [StripePaymentFormComponent]
+})
+export class StripePaymentFormModule {}
